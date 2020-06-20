713e707d5fe4e798c3476b02aff9f57179c54a2e
Dominika Cesarz <dominikacesarz@yahoo.com>
2020-06-20 19:16:23 +0200

    Cleaned out code, now we will work on test image until we are sure the alghorithm works.

--- Źródło.cpp
+++ Źródło.cpp
@@ -18,19 +18,19 @@ using namespace cv;
 //grid of image
 vector<Rect> findCells(Mat img, int GRID_SIZE)
 {
-vector<Rect> cells;
-int width = img.cols;
-int height = img.rows;
-for (int y = 0; y < height - GRID_SIZE; y += GRID_SIZE) {
-    for (int x = 0; x < width - GRID_SIZE; x += GRID_SIZE) {
-        int k = x * y + x;
-        Rect grid_rect(x, y, GRID_SIZE, GRID_SIZE);
-        cout << grid_rect << endl;
-        cells.push_back(grid_rect);
-        waitKey();
+    vector<Rect> cells;
+    int width = img.cols;
+    int height = img.rows;
+    for (int y = 0; y < height - GRID_SIZE; y += GRID_SIZE) {
+        for (int x = 0; x < width - GRID_SIZE; x += GRID_SIZE) {
+            int k = x * y + x;
+            Rect grid_rect(x, y, GRID_SIZE, GRID_SIZE);
+            cout << grid_rect << endl;
+            cells.push_back(grid_rect);
+            waitKey();
+        }
     }
-}
-return cells;
+    return cells;
 }
 
 Mat imageCorrection(Mat m) {
@@ -55,66 +55,16 @@ Mat imageCorrection(Mat m) {
 }
 
 int main() {
-    // Ró¿nica miêdzy klatkami
-    //VideoCapture cap(0, CAP_DSHOW);
-    VideoCapture cap;
-    cap.open("path");
-
-    if (!cap.isOpened()) {
-        printf("File not found.");
-        return -1;
-    }
-
-    vector<Point> coords;
-    Mat frame1g, frame1, frame2, frame2g, dif, bdif, t, h;
-    vector<vector<Point>> contours;
-    vector<Point> largest_contour;
-    vector<Vec4i> hierarchy;
-    double largest_m10;
-    double largest_m01;
-    double largest_m00;
-    int nm = 2;
-    int q = 30;
-    int b = 5;
-    int frameCount = 0;
-    int mint = 15;
-    int maxt = 100;
-    int frame_width = cap.get(CAP_PROP_FRAME_WIDTH);
-    int frame_height = cap.get(CAP_PROP_FRAME_HEIGHT);
-
-    SYSTEMTIME lt = { 0 };
-    GetLocalTime(&lt);
-    string part1 = to_string(lt.wDay) + "." + to_string(lt.wMonth) + "." + to_string(lt.wYear) + " " + to_string(lt.wHour) + "." + to_string(lt.wMinute) + "." + to_string(lt.wSecond);
-    string part2 = ".avi";
-    string file = part1 + part2;
-    VideoWriter video(file, FOURCC('XVID'), 5, Size(frame_width, frame_height), true);
+    Mat img;
+    img = imread("image.jpg");
+    vector<Rect> grid = findCells(img, 4);
 
     while (true) {
-        cap >> frame1;
-        if (frame1.empty()) break;
-        cap >> frame2;
-        if (frame2.empty()) break;
-        cvtColor(frame1, frame1g, COLOR_BGR2GRAY);
-        cvtColor(frame2, frame2g, COLOR_BGR2GRAY);
-        absdiff(frame1g, frame2g, dif);
-        if (b != 0) blur(dif, bdif, Size(b, b));
-        threshold(bdif, t, mint, maxt, THRESH_BINARY);
-        findContours(t, contours, hierarchy, RETR_LIST, CHAIN_APPROX_NONE);
-        vector<Moments> mu(contours.size());
-        if (contours.size() == 0) frameCount += 1;
-        if (contours.size() != 0) frameCount = 0;
-        if (frameCount == 90) break;
-        GetLocalTime(&lt);
-        string time = to_string(lt.wDay) + "." + to_string(lt.wMonth) + "." + to_string(lt.wYear) + " " + to_string(lt.wHour) + "." + to_string(lt.wMinute) + "." + to_string(lt.wSecond);
-        putText(frame1, time, Point(50, 50), FONT_HERSHEY_SIMPLEX, 1, Scalar(255, 255, 0));
-        video.write(frame1);
-        imshow("Frame", frame1);
+        imshow("test", img);
         int button = (char)waitKey(10);
         if (button == 27) break;
     }
 
-    video.release();
-    cap.release();
     destroyAllWindows();
     return 0;
 }
