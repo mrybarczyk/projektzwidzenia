a70ec4f3191c27fd1f53df374956334d79f4d24a
Dominika Cesarz <dominikacesarz@yahoo.com>
2020-06-23 19:57:58 +0200

    Cleaned code.

--- Źródło.cpp
+++ Źródło.cpp
@@ -17,14 +17,12 @@ using namespace std;
 using namespace cv;
 
 //grid of image
-//TODO : grid should be able to include rects that aren square if we cant get squares.
 vector<Rect> findCells(Mat img, int GRID_SIZE, int i, int j, int width, int height)
 {
     vector<Rect> cells;
-    cout << img.rows << img.cols << endl;
     for (int y = j; y < height; y += GRID_SIZE) {
         for (int x = i; x < width; x += GRID_SIZE) {
-            //int k = x * y + x;
+
             int xw = GRID_SIZE;
             int yh = GRID_SIZE;
             if (y + GRID_SIZE > height) {
@@ -34,9 +32,7 @@ vector<Rect> findCells(Mat img, int GRID_SIZE, int i, int j, int width, int heig
                 xw = width - x;
             }
             Rect grid_rect(x, y, xw, yh);
-            //cout << grid_rect << endl;
             cells.push_back(grid_rect);
-            //waitkey deleted
         }
     }
     return cells;
@@ -45,7 +41,6 @@ vector<Rect> findCells(Mat img, int GRID_SIZE, int i, int j, int width, int heig
 Mat imagePrep(Mat img) {
     // color ---> bw
     vector<vector<Point>> contours;
-    //vector<Vec4i> hierarchy;
     Mat done;
     Mat thresh;
     Size s = Size(5, 5);
@@ -57,15 +52,10 @@ Mat imagePrep(Mat img) {
     dilate(done, done, kernel, Point(-1, -1), 2, 0);
     erode(done, done, Mat(), Point(-1, -1), 1, 0);
 
-    //GaussianBlur(done, done, Point(3, 3), 1, 1, 0);
-    //erode(done, done, Mat(), Point(-1, -1), 1, 0);
-    //dilate(done, done, kernel, Point(-1, -1), 1, 0);
-    //threshold     
     threshold(done, thresh, 0, 255, THRESH_OTSU + THRESH_BINARY);
     Mat kernel2 = getStructuringElement(MORPH_RECT, Size(5, 3));
-    //i have. no idea if below will work
+    //connect letters
     dilate(thresh, thresh, kernel2, Point(-1, -1), 5, 0);
-    //morphologyEx(thresh, thresh, RETR_EXTERNAL, CHAIN_APPROX_NONE);
     //contours
     findContours(thresh, contours, RETR_EXTERNAL, CHAIN_APPROX_NONE);
     for (int i = 0; i < contours.size(); i++) {
@@ -75,8 +65,7 @@ Mat imagePrep(Mat img) {
         int h = r.height;
         int w = r.width;
         float ar = w / h;
-        if (ar >= 3) cout << "Contour found" << endl;
-        else drawContours(thresh, contours, i, Scalar(0, 0, 0), CV_FILLED);
+        if (ar < 3) drawContours(thresh, contours, i, Scalar(0, 0, 0), CV_FILLED);
         rectangle(img, r, Scalar(0, 0, 255));
     }
 
@@ -87,8 +76,7 @@ vector<Mat> findHistograms(Mat img, vector<Rect> gr) {
     vector<Mat> hists;
     Mat temp = img;
     int k = 1;
-    temp = imagePrep(temp);
-    int histSize = 256; // bin size
+    int histSize = 256;
     float range[] = { 0, 256 };
     const float* histRange = { range };
 
@@ -103,44 +91,11 @@ vector<Mat> findHistograms(Mat img, vector<Rect> gr) {
         Mat tempCropped = temp(r);
         calcHist(&tempCropped, 1, channels, Mat(), tempHist, 1, &histSize, &histRange, uniform, accumulate);
         hists.push_back(tempHist);
-        // Access histogram value of white by:
-        // tempHist.at<float>(255);
     }
     return hists;
 }
 
-void processHistogramsRedone(vector<Mat> hist1, vector<Mat> hist2, int size, int framecount) {
-    int pixelCount = size * size;
-    float curr = 0;
-    float max = 0;
-    float maxB = 0;
-    int rectCounter = 1;
-    int maxRectIndex = 0;
-    int maxRectIndexB = 0;
-    int i = 0;
-    for (i = 0; i < hist1.size(); i++)
-    {
-        float prevFrame = hist1[i].at<float>(255);
-        float currFrame = hist2[i].at<float>(255);
-        float jump = -((prevFrame / pixelCount) - (currFrame / pixelCount));
-        if (max < jump) {
-            max = jump;
-            maxRectIndex = i + 1;
-        }
-        prevFrame = hist1[i].at<float>(0);
-        currFrame = hist2[i].at<float>(0);
-        jump = -((prevFrame / pixelCount) - (currFrame / pixelCount));
-        if (maxB < jump) {
-            maxB = jump;
-            maxRectIndexB = i + 1;
-        }
-    }
-    cout << "Frame " << framecount << "has the biggest jump in quantity of white pixels at rect " << maxRectIndex << ": " << max << "%" << endl;
-    cout << "Frame " << framecount << "has the biggest jump in quantity of black pixels at rect " << maxRectIndexB << ": " << maxB << "%" << endl;
-    cout << endl << "==================================================" << endl;
-}
-
-void processHistograms(vector<Mat> hist, int size, int framecount) {
+void processHistograms(vector<Mat> hist, int size, int framecount, ofstream& filestream) {
     int pixelCount = size * size;
     float curr = 0;
     float max = 0;
@@ -155,19 +110,20 @@ void processHistograms(vector<Mat> hist, int size, int framecount) {
         rectCounter++;
     }
     float perc = max / pixelCount;
-    cout << "Frame " << framecount << "has the biggest percentage of white pixels on rect " << maxRectIndex << ": " << perc << endl;
+    if (perc > 0)
+    {
+        filestream << "Frame " << framecount << "has the biggest percentage of white pixels on rect " << maxRectIndex << ": " << perc << endl;
+        cout << "Frame " << framecount << "has the biggest percentage of white pixels on rect " << maxRectIndex << ": " << perc << endl;
+    }
 }
 
 int main() {
-
-    // EXAMPLE OF CORRECT APPLICATION OF FILTER TO ONLY ONE RECT
-    //GaussianBlur(img(grid[1]), img(grid[1]), Point(101, 101), 5, 5, 0);
     VideoCapture cap("ad4.mp4");
-    Mat img1, img2;
-    Mat resized1(Size(990, 540), CV_64FC1);
-    Mat resized2(Size(990, 540), CV_64FC1);
-    ofstream f;
-    f.open("file.txt");
+    Mat img;
+    Mat resized(Size(990, 540), CV_64FC1);
+    ofstream f1, f2;
+    f1.open("subtitles_detection.txt");
+    f2.open("white_pixels_percentages.txt");
     if (!cap.isOpened())
     {
         cout << "Nie mozna by³o odtworzyæ video.";
@@ -179,79 +135,71 @@ int main() {
     int width = 990;
     int frameCount = 1;
     while (true) {
-        cap >> img1;
-        if (img1.empty()) {
-            return 0;
-        }
-        cap >> img2;
-        if (img2.empty()) {
+        cap >> img;
+        if (img.empty()) {
             return 0;
         }
         boolean detected = false;
         int sizecounter = 0;
-        resize(img1, resized1, resized1.size(), 0, 0, 1);
-        resize(img2, resized2, resized2.size(), 0, 0, 1);
-        Mat temp1 = imagePrep(resized1);
-        Mat temp2 = imagePrep(resized2);
-        vector<Rect> grid1 = findCells(resized1, 80, x, y, width, height);
-        vector<Rect> grid2 = findCells(resized2, 80, x, y, width, height);
-        vector<Mat> hists1 = findHistograms(resized1, grid1);
-        vector<Mat> hists2 = findHistograms(resized2, grid2);
-        for (Rect r : grid1)
-            rectangle(resized1, r, (255, 255, 255), 1, 8, 0);
-        for (Rect r : grid2)
-            rectangle(resized2, r, (255, 255, 255), 1, 8, 0);
-        //Mat temp = imagePrep(img);
+        resize(img, resized, resized.size(), 0, 0, 1);
+        Mat temp = imagePrep(resized);
+        vector<Rect> grid = findCells(resized, 120, x, y, width, height);
+        vector<Mat> hists = findHistograms(temp, grid);
+        for (Rect r : grid)
+            rectangle(resized, r, (255, 255, 255), 1, 8, 0);
+
         int button = (char)waitKey(10);
         if (button == 27) break;
         // s
         if (button == 119 && y >= 50) y -= 50;
         // w
-        if (button == 115 && y < resized1.rows-50) y += 50;
+        if (button == 115 && y < resized.rows - 50) y += 50;
         // d
-        if (button == 100 && x < resized1.cols-50) x += 50;
+        if (button == 100 && x < resized.cols - 50) x += 50;
         // a
         if (button == 97 && x >= 50) x -= 50;
         // z - smaller width
         if (button == 122 && width >= 50) width -= 50;
         // x - bigger width
-        if (button == 120 && width <= resized1.cols-50) width += 50;
+        if (button == 120 && width <= resized.cols - 50) width += 50;
         // c - smaller height
         if (button == 99 && height >= 50) height -= 50;
         // v - bigger height
-        if (button == 118 && height <= resized1.rows-50) height -= 50;
-        processHistogramsRedone(hists1, hists2, 80, frameCount);
+        if (button == 118 && height <= resized.rows - 50) height += 50;
+        processHistograms(hists, 120, frameCount, f2);
         int i = 1;
-        for (Rect r : grid1) {
-            rectangle(resized1, r, (255, 255, 255), 1, 8, 0);
-            putText(resized1(r), to_string(i), Point(0, 45),
+        for (Rect r : grid) {
+            rectangle(resized, r, (255, 255, 255), 1, 8, 0);
+            putText(resized(r), to_string(i), Point(0, 45),
                 cv::FONT_HERSHEY_DUPLEX,
                 1,
                 CV_RGB(118, 185, 0), //font color
                 2);
             i++;
         }
-        putText(resized1, to_string(frameCount), Point(20, 80),
+        putText(resized, to_string(frameCount), Point(20, 80),
             cv::FONT_HERSHEY_DUPLEX,
             2,
             CV_RGB(255, 255, 255), //font color
             2);
-        //processHistograms(hists, 80, frameCount);
         for (int i = x; i < height; i++) {
             for (int j = y; j < width; j++) {
-                Scalar color = temp1.at<uchar>(i, j);
+                Scalar color = temp.at<uchar>(i, j);
                 if (color[0] == 255) sizecounter += 1;
             }
         }
-        if ((float)((float)sizecounter / (float)(temp1.rows*temp1.cols)) >= 0.03) detected = true;
+        if ((float)((float)sizecounter / (float)(temp.rows * temp.cols)) >= 0.03) detected = true;
         if (detected) {
-            f << "Subtitles detected: " << to_string(frameCount) << "\n";
+            f1 << "Subtitles detected: " << to_string(frameCount) << "\n";
+            cout << "Subtitles detected: " << to_string(frameCount) << endl;
+            cout << "=======================" << endl;
         }
-        imshow("test", temp1);
-        imshow("resized", resized1);
+        imshow("test", temp);
+        imshow("resized", resized);
         frameCount++;
     }
-    f.close();
+    f1.close();
+    f2.close();
     destroyAllWindows();
     return 0;
 }
